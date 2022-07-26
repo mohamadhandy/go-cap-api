483e66f97245748c90f656a99ae80ff61c12c723
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-07-26 16:18:15 +0700

    adding update and delete endpoint

--- app/app.go
+++ app/app.go
@@ -12,9 +12,12 @@ func Start() {
 
 	// * defining routes
 	mux.HandleFunc("/greet", Greet).Methods(http.MethodGet)
+	mux.HandleFunc("/customers", AddCustomer).Methods(http.MethodPost)
 
 	mux.HandleFunc("/customers", GetCustomers).Methods(http.MethodGet)
-	mux.HandleFunc("/customers/{customer_id}", GetCustomer).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", GetCustomer).Methods(http.MethodGet)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", DeleteCustomer).Methods(http.MethodDelete)
+	mux.HandleFunc("/customers/{customer_id:[0-9]+}", UpdateCustomer).Methods(http.MethodPut)
 
 	// * starting the server
 	http.ListenAndServe(":8080", mux)
--- app/handlers.go
+++ app/handlers.go
@@ -3,6 +3,7 @@ package app
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -31,6 +32,7 @@ func Greet(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetCustomers(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetCustomers")
 	if r.Header.Get("Content-Type") == "application/xml" {
 		w.Header().Add("Content-Type", "application/xml")
 		xml.NewEncoder(w).Encode(customers)
@@ -41,6 +43,7 @@ func GetCustomers(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getcustomer")
 	customer := mux.Vars(r)
 	customerId, _ := strconv.Atoi(customer["customer_id"])
 
@@ -50,6 +53,73 @@ func GetCustomer(w http.ResponseWriter, r *http.Request) {
 			getCustomer = v
 		}
 	}
+
+	if getCustomer.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "customer data not found")
+		return
+	}
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(getCustomer)
+}
+
+func AddCustomer(w http.ResponseWriter, r *http.Request) {
+	var cust Customer
+	json.NewDecoder(r.Body).Decode(&cust)
+
+	nextId := getNextID()
+	cust.ID = nextId
+
+	customers = append(customers, cust)
+	w.WriteHeader(http.StatusCreated)
+	log.Println("Add customer successfully")
+}
+
+func DeleteCustomer(w http.ResponseWriter, r *http.Request) {
+	customer := mux.Vars(r)
+	customerId, _ := strconv.Atoi(customer["customer_id"])
+
+	var counter int = 0
+	for index, v := range customers {
+		if v.ID == customerId {
+			customers = append(customers[:index], customers[index+1:]...)
+			counter++
+		}
+	}
+	if counter > 0 {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprint(w, "Remove customer successfully")
+	} else {
+		fmt.Fprint(w, "Delete customer unsuccessful, customer not found")
+	}
+}
+
+func UpdateCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("UpdateCustomer")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	cust_id, _ := strconv.Atoi(params["customer_id"])
+	var counter int = 0
+	for index, item := range customers {
+		if item.ID == cust_id {
+			customers = append(customers[:index], customers[index+1:]...)
+			var customerData Customer
+			_ = json.NewDecoder(r.Body).Decode(&customerData)
+			customerData.ID = cust_id
+			customers = append(customers, customerData)
+			json.NewEncoder(w).Encode(customerData)
+			counter++
+		}
+	}
+	if counter > 0 {
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprint(w, "Update customer successfully")
+	} else {
+		fmt.Fprint(w, "Update customer unsuccessful, customer not found")
+	}
+}
+
+func getNextID() int {
+	cust := customers[len(customers)-1]
+	return cust.ID + 1
 }
