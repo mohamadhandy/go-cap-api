137c0321c1efb5a5c785dab0dd4ea54e3d913823
Mohamad Handy Nugraha <handynugraha29@gmail.com>
2022-07-29 21:17:07 +0700

    create environment variable

--- app/app.go
+++ app/app.go
@@ -2,13 +2,26 @@ package app
 
 import (
 	"capi/domain"
+	"capi/logger"
 	"capi/service"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
+func sanityCheck() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func Start() {
+	sanityCheck()
 	ch := CustomerHandler{service.NewCustomerService(domain.NewCustomerRepositoryDB())}
 
 	mux := mux.NewRouter()
@@ -23,5 +36,8 @@ func Start() {
 	// mux.HandleFunc("/customers/{customer_id:[0-9]+}", UpdateCustomer).Methods(http.MethodPut)
 
 	// * starting the server
-	http.ListenAndServe(":8080", mux)
+	serverAddr := os.Getenv("SERVER_ADDRESS")
+	serverPort := os.Getenv("SERVER_PORT")
+	logger.Info(fmt.Sprintf("Start server on %s:%s ...", serverAddr, serverPort))
+	http.ListenAndServe(fmt.Sprintf("%s:%s", serverAddr, serverPort), mux)
 }
